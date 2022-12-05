96ca31b3ceaf4fb748f254ba4a31ad2d464db7ca
Roman Triputko <romantriputko@gmail.com>
2022-12-05 06:12:31 +0100

    changes naming for env variables

--- backend/src/config/env.schema.ts
+++ backend/src/config/env.schema.ts
@@ -2,10 +2,10 @@ import {IsNumber, IsString} from "class-validator";
 
 export class EnvironmentVariables {
     @IsNumber()
-    JWT_EXPIRING_MS: number
+    ACCESS_TOKEN_EXPIRING_MS: number
 
     @IsString()
-    JWT_SECRET_KEY: string;
+    ACCESS_TOKEN_SECRET_KEY: string;
 
     @IsString()
     MONGO_DB_CONNECT_LINK: string
--- backend/src/config/jwt/jwt-configuration.configuration.ts
+++ backend/src/config/jwt/jwt-configuration.configuration.ts
@@ -2,7 +2,7 @@ import {JwtConfigurationInterface} from "./jwt-configuration.interfaces";
 
 export const jwtConfiguration = (): JwtConfigurationInterface => ({
     jwt: {
-        secret: process.env.JWT_SECRET_KEY!,
-        expiringMs: parseInt(process.env.JWT_EXPIRING_MS!, 10),
+        secret: process.env.ACCESS_TOKEN_SECRET_KEY!,
+        expiringMs: parseInt(process.env.ACCESS_TOKEN_EXPIRING_MS!, 10),
     }
 })
